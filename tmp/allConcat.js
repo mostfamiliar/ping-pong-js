1e1911ff173333e5d53e80abb9d2153234dcd254
torrence stratton <tlstratton@gmail.com>
2016-03-14 22:30:01 -0700

    add API key and fix jsBuild issue

--- tmp/allConcat.js
+++ tmp/allConcat.js
@@ -19,3 +19,17 @@ $(document).ready(function(){
     $('#solution').prepend('<p>Thank you, ' + email + ' has been added to our list!</p>');
   });
 });
+
+var apiKey = "10d968b7887d0a0881bc8c572b93b271";
+
+$(document).ready(function() {
+  $('#weatherLocation').click(function() {
+    var city = $('#location').val();
+    $('#location').val("");
+    $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey).then(function(response) {
+      $('.showWeather').text("The humidity in " + city + " is " + response.main.humidity + "%");
+    }).fail(function(error) {
+      $('.showWeather').text(error.message);
+    });
+  });
+});
